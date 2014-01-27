fb1433c51cace3eafb8601753b5c205d9086a769
Jeth <jethdeguzman@gmail.com>
2014-01-27 23:54:32 +0800

    login

---
+++ assets/www/assets/js/default.js
@@ -0,0 +1,98 @@
+	//check internet connection of user thru server request
+	function checkInternet(){
+		//return true if internet is ok	
+		//return false if intenet is not ok
+		var x = false;
+		$.ajax({
+			url : 'http://rescuemakati.cloudapp.net/check',
+			type : "GET",
+			dataType: "JSON",
+			async : false,
+			success: function(data){
+				x = true;
+
+			},
+			error : function(err){
+				x =  false;
+			}
+		});
+		return x;
+	}
+
+	function updateInfoAlert(){
+		alert("Please Update your Personal Details");
+		// location.href = "#tab2";
+		// $$("#tab2").addClass('active');
+		$(".home-div").hide();	
+		$(".hotlines-div").hide();	
+		$(".info-div").show();	
+	}
+
+	//show home tab
+	function showHome(x){
+		$(".info-div").hide();	
+		$(".hotlines-div").hide();	
+		$(".home-div").show();
+		$(".tab").parent().removeClass("active");
+		$(x).parent().addClass("active");
+		initialize(); //map initialization
+	}
+
+	//show info tab
+	function showInfo(x){
+		$(".home-div").hide();	
+		$(".hotlines-div").hide();	
+		$(".info-div").show();	
+		$(".tab").parent().removeClass("active");
+		$(x).parent().addClass("active");
+	}
+
+	//show hotlines tab
+	function showHotlines(x){
+		$(".home-div").hide();	
+		$(".info-div").hide();	
+		$(".hotlines-div").show();
+		$(".tab").parent().removeClass("active");
+		$(x).parent().addClass("active");
+	}
+
+	//sending emergency request to server
+	/**
+		request = emergency button
+	**/
+	function sendEmergency(request){
+		// alert('sending');
+		var server = "http://rescuemakati.cloudapp.net/request/add";
+		var server2 = "http://192.168.1.105:1337/request/add";
+		var data = JSON.stringify({request : request, userid : getUserInfo().userid, name : getUserInfo().firstname+' '+getUserInfo().lastname, age : getUserInfo().age, mobile : getUserInfo().mobile, lat : getMapInfo().lat, lng : getMapInfo().lng, address : getMapInfo().address });
+		
+		$.ajax({
+			url : server,
+			type : "POST",
+			data : {
+					data : data
+			},
+			success : function(data){
+				alert(data.status);
+			},
+			error : function(err){
+				console.log(err);
+				alert('error');
+			}
+		});
+	}
+	//setting user info 
+	function setUserInfo(userid, firstname, lastname, age, mobile){
+		var userinfo = {userid : userid, firstname : firstname, lastname : lastname, age : age, mobile : mobile};
+		localStorage.userinfo = JSON.stringify(userinfo);
+	}
+
+
+	//retrieve Map info from gmaps.js
+	function getMapInfo(){
+		return {lat : lat, lng : lng, address : address};
+	}
+	//retrieve user info from localStorage.userinfo
+	function getUserInfo(){
+		return JSON.parse(localStorage.userinfo);
+	}
--- src/com/example/rescuemakati/MainActivity.java
+++ src/com/example/rescuemakati/MainActivity.java
@@ -8,10 +8,10 @@ public class MainActivity extends DroidGap{
 
     @Override          
 	public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); 
-        super.loadUrl("file:///android_asset/www/index.html");
-    }                  
-                                                                    
-                                                       
-     }                                               	                        
-                         
+        super.onCreate(savedInstanceState);       
+        super.loadUrl("file:///android_asset/www/login.html");
+    }                      
+                                                                                   
+                                                                                   
+     }                                                      	                        
+                                                                          
