807c7d1c5f4ed37b5489ac33c555b65c31897c2c
Jeth <jethdeguzman@gmail.com>
2014-02-06 11:45:09 +0800

    polish

--- src/com/example/makati168/MainActivity.java
+++ src/com/example/makati168/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends DroidGap{
 	public void onCreate(Bundle savedInstanceState) {
     	
                                                           
-        super.onCreate(savedInstanceState); 
+        super.onCreate(savedInstanceState);         
                                                                              
         super.loadUrl("file:///android_asset/www/login.html");
     }                      
